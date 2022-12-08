5a747ba92b7a629f60609bc11de527e5721e6ce8
Adela Viskova <av1322@imperial.ac.uk>
2022-12-08 15:38:55 +0000

    Exits added after errors

--- Makefile
+++ Makefile
@@ -4,11 +4,8 @@ all: main
 main: helper.o main.o queue.o
 	$(CC) -pthread -o main helper.o main.o queue.o
 
-main.o: helper.cc main.cc queue.o
+main.o: helper.cc main.cc queue.cc
 	$(CC) -c helper.cc main.cc queue.cc
-
-queue.o: queue.cc queue.h
-	$(CC) -c queue.cc
 
 tidy:
 	rm -f *.o core
--- main.cc
+++ main.cc
@@ -31,8 +31,16 @@ int main (int argc, char **argv) {
   
   // check if right number of arguments provided
   if(argc != 5){
-    cerr << "Number of arguments must be four!" << endl;
-    return 0;
+    printf("Error: Number of arguments must be four!\n");
+    exit(1);
+  }
+
+  // check if the arguments are positive
+  for(int i=1; i<5; i++){
+    if(check_arg(argv[i])<0){
+      printf("Error: Arguments must be integers equal to or larger than 0!\n");
+      exit(1);
+    }
   }
 
   // store the command line arguments
@@ -53,18 +61,34 @@ int main (int argc, char **argv) {
 
   // Create and initialise semaphores
   semID = sem_create(SEM_KEY, semaphores_no);
-  sem_init(semID, MUTEX, 1);
-  sem_init(semID, EMPTY, queue_size);
-  sem_init(semID, FULL, 0);
+  if(semID==-1){
+    printf("Error: Failed to create semaphore array.\n");
+    printf("Make sure you have deleted previously used "
+        "semaphores; use the Unix command 'ipcrm -a' \n");
+    exit(1);
+  }
+  if(sem_init(semID, MUTEX, 1)==-1){
+    printf("Error: Failed to initialise MUTEX semaphore.\n");
+    exit(1);
+  }
+  if(sem_init(semID, EMPTY, queue_size)==-1){
+    printf("Error: Failed to initialise EMPTY semaphore.\n");
+    exit(1);
+  }
+  if(sem_init(semID, FULL, 0)==-1){
+    printf("Error: Failed to initialise FULL semaphore.\n");
+    exit(1);
+  }
   
   // create producer threads
   pthread_t producerid[num_producers];
   int prod_id[num_producers];
   for(int n=0; n<num_producers; n++){
     prod_id[n]=n+1;
-    if((pthread_create(&producerid[n], NULL, producer, (void*)&prod_id[n])!=0)){
+    if(pthread_create(&producerid[n], NULL, producer, (void*)&prod_id[n])!=0){
       // unsuccessful creation of thread
-      perror("Producer thread creation unsuccessful\n");
+      printf("Error: Failed to create producer thread.\n");
+      exit(1);
     }
   }
 
@@ -73,9 +97,10 @@ int main (int argc, char **argv) {
   int con_id[num_consumers];
   for(int n=0; n<num_consumers; n++){
     con_id[n]=n+1;
-    if((pthread_create(&consumerid[n], NULL, consumer, (void*)&con_id[n])!=0)){
+    if(pthread_create(&consumerid[n], NULL, consumer, (void*)&con_id[n])!=0){
       // unsuccessful creation of thread
-      perror("Consumer thread creation unsuccessful\n");
+      printf("Error: Failed to create consumer thread.\n");
+      exit(1);
     }
   }
 
@@ -83,17 +108,22 @@ int main (int argc, char **argv) {
   for (int n=0; n<num_producers; n++){
     if((pthread_join(producerid[n], NULL))!=0){
       // unsuccessful  join
-      perror("Producer thread join unsuccessful\n");
+      printf("Error: Failed to join producer thread.\n");
+      exit(1);
     }
   }
   for (int n=0; n<num_consumers; n++){
     if((pthread_join(consumerid[n], NULL))!=0){
       // unsuccessful  join
-      perror("Consumer thread join unsuccessful\n");
+      printf("Error: Failed to join consumer thread.\n");
+      exit(1);
     }
   }
   // clean up 
-  sem_close(semID);
+  if(sem_close(semID)==-1){
+    // unsuccessful clean up
+    printf("Error: Failed to close semaphore array.\n");
+  }
   delete circQ;
 
   // time taken 
@@ -107,7 +137,6 @@ int main (int argc, char **argv) {
 
 void *producer (void *pnumber) {
   
-  int wait_return;
   int job_n=0;
 
   while(job_n<n_of_jobs){
@@ -121,21 +150,18 @@ void *producer (void *pnumber) {
     time_s.tv_sec=MAX_WAIT;
     
     // wait if queue is not empty (wait maximum of 20s)
-    while((wait_return=sem_timed_wait(semID, EMPTY, &time_s))==-1 && 
-           errno == EINTR)
-      continue; // restart if signal handler interrupt
-
     // if timed_wait unsuccessful:
-    if((wait_return==-1)) { 
+    if(sem_timed_wait(semID, EMPTY, &time_s)==-1) { 
       if (errno == EAGAIN){ // time limit (20s) expired
-        printf("Producer(%d): No slot in queue empty after 20s."
+        fprintf(stderr, "Producer(%d): No slot in queue after 20s."
         " Producer quitting!\n",*((int*)pnumber));
-        pthread_exit((void*) 0);
+        pthread_exit(0);
       }
       else{
-        printf("Make sure you have deleted previously used semaphores;");
-        printf(" use the Unix command 'ipcrm -a' \n");
-        pthread_exit((void*) 0);
+        perror("Error");
+        fprintf(stderr,"Error: Make sure you have deleted previously used "
+        "semaphores; use the Unix command 'ipcrm -a' \n");
+        exit(1);
       }
     }
     // if timed_wait successful:
@@ -145,7 +171,7 @@ void *producer (void *pnumber) {
       // Add the job to the circular queue
       int job_id = circQ->get_end();
       circQ->addElement(job_duration);
-      printf("Producer(%d): Job id %d duration %d\n",\
+      fprintf(stderr, "Producer(%d): Job id %d duration %d\n",\
       *((int*)pnumber), job_id, job_duration);
       /*----------- leaving the critical section -----------*/
       sem_signal(semID, MUTEX);
@@ -153,34 +179,30 @@ void *producer (void *pnumber) {
       job_n++;
     }
   }
-  printf("Producer(%d): No more jobs to generate.\n", *((int*)pnumber));
-  pthread_exit((void*) 0);
+  fprintf(stderr,"Producer(%d): No more jobs to generate.\n", *((int*)pnumber));
+  pthread_exit(0);
 }
 
 /****************************** CONSUMER ******************************/
 
 void *consumer (void *cnumber) {
-  
-  int wait_return;
 
   while(1){
     // set the number of seconds of the time_s structure to MAX_WAIT (20s)
     time_s.tv_sec=MAX_WAIT; 
-    // wait if queue is not filled (wait maximum of 20s)
-    while((wait_return = sem_timed_wait(semID, FULL, &time_s))==-1 && 
-           errno == EINTR)
-      continue; // restart if signal handler interrupt
 
+    // wait if queue is not filled (wait maximum of 20s)
     // if timed_wait unsuccessful:  
-    if(wait_return==-1){
+    if(sem_timed_wait(semID, FULL, &time_s)==-1){
       if (errno == EAGAIN){ // time limit (20s) expired
-        printf("Consumer(%d): No more jobs left.\n", *((int*)cnumber));
-        pthread_exit((void*) 0);
+        fprintf(stderr, "Consumer(%d): No more jobs left.\n", *((int*)cnumber));
+        pthread_exit(0);
       }
       else{
-        printf("Make sure you have deleted previously used semaphores;");
-        printf(" use the Unix command 'ipcrm -a' \n");
-        pthread_exit((void*) 0);
+        perror("Error:");
+        fprintf(stderr,"Error: Make sure you have deleted previously used "
+        "semaphores; use the Unix command 'ipcrm -a' \n");
+        exit(1);
       }
     }
     // if timed_wait successful:
@@ -190,15 +212,16 @@ void *consumer (void *cnumber) {
       // consume a job 
       int job_id = circQ->get_start();
       int job_consumed = circQ->deleteElement();
-      printf("Consumer(%d): Job id %d executing sleep duration %d\n",\
+      fprintf(stderr, "Consumer(%d): Job id %d executing sleep duration %d\n",\
       *((int*)cnumber), job_id, job_consumed);
       /*----------- leaving the critical section -----------*/
       sem_signal(semID, MUTEX);
       sem_signal(semID, EMPTY);
       // sleep for time it takes to process the job 
       sleep(job_consumed);
-      printf("Consumer(%d): Job id %d completed\n", *((int*)cnumber), job_id);
+      fprintf(stderr, "Consumer(%d): Job id %d completed\n",\
+       *((int*)cnumber), job_id);
     }
   }
-  pthread_exit((void*) 0);
+  pthread_exit(0);
 }
